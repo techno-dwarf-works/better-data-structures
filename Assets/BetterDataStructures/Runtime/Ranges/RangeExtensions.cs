a24cbf5eb53a76774372ae48edab47167ff4e354
uurha <uurhaa@gmail.com>
2023-04-10 21:13:24 +0200

    Fix for Unity 2020

--- Assets/BetterDataStructures/Runtime/Ranges/RangeExtensions.cs
+++ Assets/BetterDataStructures/Runtime/Ranges/RangeExtensions.cs
@@ -9,20 +9,37 @@ namespace Better.DataStructures.Ranges
         {
             return Mathf.Clamp(value, range.Min, range.Max);
         }
-        
+
         public static float Clamp(this Range<float> range, float value)
         {
             return Mathf.Clamp(value, range.Min, range.Max);
         }
-        
+
         public static double Clamp(this Range<double> range, double value)
         {
+#if UNITY_2021_3_OR_NEWER
+
             return Math.Clamp(value, range.Min, range.Max);
+#else
+            if (value < range.Min)
+                value = range.Min;
+            else if (value > range.Max)
+                value = range.Max;
+            return value;
+#endif
         }
-        
+
         public static byte Clamp(this Range<byte> range, byte value)
         {
+#if UNITY_2021_3_OR_NEWER
             return Math.Clamp(value, range.Min, range.Max);
+#else
+            if (value < range.Min)
+                value = range.Min;
+            else if (value > range.Max)
+                value = range.Max;
+            return value;
+#endif
         }
     }
 }
